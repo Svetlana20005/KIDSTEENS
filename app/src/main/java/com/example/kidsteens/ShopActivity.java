0d3aadbf8f34475c480beef8888dd21ef1214dc6
Svetlana20005 <svetavas20005@mail.ru>
2023-05-18 14:03:09 +0300

    shopActivity

--- app/src/main/java/com/example/kidsteens/ShopActivity.java
+++ app/src/main/java/com/example/kidsteens/ShopActivity.java
@@ -3,14 +3,25 @@ package com.example.kidsteens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 
 public class ShopActivity extends AppCompatActivity {
+    Spinner spinner;
+    ArrayList<Category> categories;
+    ArrayList<Product> products;
+    DBCategories dbCategories;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+        spinner = findViewById(R.id.spinner);
+        dbCategories = new DBCategories(getBaseContext());
+        categories = dbCategories.getAll();
+        spinner.setAdapter(new ArrayAdapter<Category>(getBaseContext(), android.R.layout.simple_list_item_1, categories));
+        spinner.setSelection(-1);
     }
 }
